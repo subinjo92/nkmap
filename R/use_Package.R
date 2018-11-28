b3c6735c652ab6f941e24c42a69abd9fe766fc33
subinjo92 <vgd2469@gmail.com>
2018-11-28 16:27:35 +0900

    name changed

--- R/use_Package.R
+++ R/use_Package.R
@@ -1,5 +1,5 @@
 #package의 설치와 불러오는것을 동시에 해주는 함수
-use_Package <- function (package, use = TRUE) {
+use_package <- function (package, use = TRUE) {
   if (!(package %in% installed.packages()[,'Package']))
     install.packages(package, dep = TRUE)
   if (use)
