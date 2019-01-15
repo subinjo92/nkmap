211c3ba72406112c6400ed0d588c2ef4a2917613
subinjo92 <vgd2469@gmail.com>
2019-01-15 22:30:34 +0900

    help update

--- R/firestation_data.R
+++ R/firestation_data.R
@@ -5,9 +5,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(firestation)
-#'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#' @usage data(firestation, package = 'nkmap')
 #'
 #' @keywords datasets
 #'
@@ -15,9 +13,5 @@
 #'
 #' @examples
 #' data(firestation)
-#' \donttest{
-#' iplotCurves(phe, times, phe[,c(61,121)], phe[,c(121,181)],
-#'             chartOpts=list(curves_xlab="Time (hours)", curves_ylab="Root tip angle (degrees)",
-#'                            scat1_xlab="Angle at 2 hrs", scat1_ylab="Angle at 4 hrs",
-#'                            scat2_xlab="Angle at 4 hrs", scat2_ylab="Angle at 6 hrs"))}
+
 "firestation"
